736815e04876affb7bd94cb7676367dc740c7939
Anish Khanijaur <anishkhanijaur089@gmail.com>
2024-03-12 22:44:42 +0700

    feat: Added login page and added /login path to login page in vue router

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <template>
-  <hello-world />
+  <v-container>
+    <h2>Home</h2>
+    <ul>
+      <li>
+        <router-link to="/">Go to Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">Go to About</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Go to Login</router-link>
+      </li>
+    </ul>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
