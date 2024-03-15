7f23c4181d7be026b8c5fadbad196a851355e0d2
Asa <92502339+Singh1403@users.noreply.github.com>
2024-03-15 11:53:02 +0700

    feat: Login Looks sexier

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,25 +1,64 @@
 <template>
   <v-container>
-    <v-form ref="form">
-      <v-text-field
-        v-model="username"
-        :rules="usernameRules"
-        label="Username"
-        required
-      ></v-text-field>
-
-      <v-text-field
-        v-model="password"
-        :rules="passwordRules"
-        label="Password"
-        type="password"
-        required
-      ></v-text-field>
-
-      <v-btn color="success" class="mr-4" @click="validate"> Login</v-btn>
-
-      <v-btn color="error" class="mr-4" @click="reset"> Reset</v-btn>
-    </v-form>
+    <v-row justify="center" align="center" class="fill-height">
+      <v-col cols="12" sm="8" md="4">
+        <!-- Flip card container -->
+        <transition name="flip" mode="out-in">
+          <div :key="flipKey">
+            <!-- Login Form -->
+            <v-form v-if="!showSignup" ref="form" class="pt-6">
+              <v-text-field
+                v-model="username"
+                :rules="usernameRules"
+                label="Username"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+              <v-btn color="success" class="mr-4" @click="validate"
+                >Login</v-btn
+              >
+              <v-btn color="error" class="mr-4" @click="reset">Reset</v-btn>
+              <v-btn text @click="toggleForm">Sign Up</v-btn>
+            </v-form>
+            <!-- Signup Form -->
+            <v-form v-else ref="signupForm" class="pt-6">
+              <v-text-field
+                v-model="newUsername"
+                :rules="usernameRules"
+                label="Username"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="newEmail"
+                :rules="emailRules"
+                label="Email"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="newPassword"
+                :rules="passwordRules"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+              <v-btn color="primary" class="mr-4" @click="signup"
+                >Sign Up</v-btn
+              >
+              <v-btn color="error" class="mr-4" @click="resetSignupForm"
+                >Reset</v-btn
+              >
+              <v-btn text @click="toggleForm">Back to Login</v-btn>
+            </v-form>
+          </div>
+        </transition>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -28,12 +67,19 @@ import Vue from "vue";
 
 export default {
   data: () => ({
+    showSignup: false,
+    flipKey: 0,
     username: "",
+    newUsername: "",
+    newEmail: "",
+    newPassword: "",
     usernameRules: [(v) => !!v || "Username is required"],
-    password: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
     passwordRules: [(v) => !!v || "Password is required"],
   }),
-
   methods: {
     async validate() {
       if (this.$refs.form.validate()) {
@@ -46,10 +92,41 @@ export default {
         }
       }
     },
+    async signup() {
+      if (this.$refs.signupForm.validate()) {
+        let formData = new FormData();
+        formData.append("username", this.newUsername);
+        formData.append("email", this.newEmail);
+        formData.append("password", this.newPassword);
+        // let response = await Vue.axios.post("/api/signup", formData);
+        // Handle signup response here (e.g., show a success message, redirect, etc.)
+      }
+    },
     reset() {
       this.username = "";
       this.password = "";
     },
+    resetSignupForm() {
+      this.newUsername = "";
+      this.newEmail = "";
+      this.newPassword = "";
+    },
+    toggleForm() {
+      this.showSignup = !this.showSignup;
+      this.flipKey++; // Increment key to trigger flip animation
+      this.reset();
+      this.resetSignupForm();
+    },
   },
 };
 </script>
+
+<style>
+.flip-enter-active,
+.flip-leave-active {
+  transition: transform 0.5s;
+}
+.flip-enter, .flip-leave-to /* .flip-leave-active in <2.1.8 */ {
+  transform: rotateY(180deg);
+}
+</style>
