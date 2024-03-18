f24be636d961cf0241aa22ad6c7560b03d63d363
Nate-81 <nathans8100@gmail.com>
2024-03-18 22:11:21 +0700

    feat: Graph doesn't all work, no major errors on frontend

--- frontend/src/views/AnalyticsView.vue
+++ frontend/src/views/AnalyticsView.vue
@@ -8,6 +8,7 @@
       <!--      Sidebar for viewing income/expenditure by category-->
       <div class="sidebar">
         <div class="amount">
+          <h2>Amount:</h2>
           <h2>{{ totalAmount }}</h2>
           <!--          Displays the amount-->
         </div>
@@ -26,28 +27,114 @@
         </div>
         <div class="tag-stats">
           <h2>Tag Stats</h2>
+          <!--          Lists down all the tags and how much income and expenditure has been done on them-->
+          <!--          represented with a bar -->
           <ul>
             <li v-for="(value, key) in tagStats" :key="key">
               Tag: {{ key }}, Amount Spent: {{ value[1] }}
             </li>
           </ul>
-
-          <!--          Lists down all the tags and how much income and expenditure has been done on them-->
-          <!--          represented with a bar -->
         </div>
       </div>
-
       <!--      Main content section-->
       <div class="content">
         <!-- Filters section -->
         <div class="filters">
           <h2>Filters</h2>
+          <div class="top-row">
+            <label>
+              Start Date:
+              <input type="date" v-model="startDate" placeholder="Start Date" />
+            </label>
+            <label>
+              End Date:
+              <input type="date" v-model="endDate" placeholder="End Date" />
+            </label>
+            <label>
+              Chart Type:
+              <select v-model="chartType">
+                <option value="bar">Bar Chart</option>
+                <option value="line">Line Chart</option>
+              </select>
+            </label>
+          </div>
+          <div class="bottom-row">
+            <label>
+              Transaction Type:
+              <select v-model="transactionType">
+                <option value="EXPENDITURE">Expenses</option>
+                <option value="INCOME">Income</option>
+              </select>
+            </label>
+            <label>
+              Tag 1:
+              <select v-model="selectedTag1">
+                <option v-for="(value, key) in tags" :key="key" :value="key">
+                  {{ key }}
+                </option>
+              </select>
+            </label>
+            <label>
+              Tag 2:
+              <select v-model="selectedTag2">
+                <option v-for="(value, key) in tags" :key="key" :value="key">
+                  {{ key }}
+                </option>
+              </select>
+            </label>
+            <label>
+              Timeframe:
+              <select v-model="timeframe">
+                <option value="Day">Day</option>
+                <option value="Month">Month</option>
+                <option value="Year">Year</option>
+              </select>
+            </label>
+          </div>
+          <div class="third-row">
+            <button @click="toggleCompare">{{ compareText }}</button>
+            <button @click="createChart">Create</button>
+          </div>
           <!-- Filters content... -->
         </div>
-
+        <div v-if="showFilters">
+          <h3>Compare With</h3>
+          <div class="bottom-row">
+            <label>
+              Transaction Type:
+              <select v-model="transactionType">
+                <option value="EXPENDITURE">Expenses</option>
+                <option value="INCOME">Income</option>
+              </select>
+            </label>
+            <label>
+              Tag 1:
+              <select v-model="selectedTag1">
+                <option v-for="(value, key) in tags" :key="key" :value="key">
+                  {{ key }}
+                </option>
+              </select>
+            </label>
+            <label>
+              Tag 2:
+              <select v-model="selectedTag2">
+                <option v-for="(value, key) in tags" :key="key" :value="key">
+                  {{ key }}
+                </option>
+              </select>
+            </label>
+          </div>
+        </div>
         <!-- Graph section -->
         <div class="graph">
           <h2>Graph</h2>
+          <!--          <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />-->
+          <p>
+            {{ startDate }}<br />{{ endDate }}<br />{{ chartType }}<br />{{
+              transactionType
+            }}<br />{{ timeframe }}
+          </p>
+          <p>Printing Graph data: {{ graphData }}</p>
           <!-- Graph content... -->
         </div>
       </div>
@@ -56,13 +143,50 @@
 </template>
 
 <script setup>
-import NavbarComponent from "@/components/NavbarComponent.vue";
-import { computed, ref, watch } from "vue";
+// eslint-disable-next-line no-unused-vars
+import { computed, onMounted, ref, watch } from "vue";
 import axios from "axios";
-// import axios from "axios";
+import NavbarComponent from "@/components/NavbarComponent.vue";
+
+const currentDate = new Date();
+const startOfYear = new Date(currentDate.getFullYear(), 0, 2);
+const startDate = ref(startOfYear.toISOString().split("T")[0]);
+const endDate = ref(currentDate.toISOString().split("T")[0]);
+const chartType = ref("line");
+// const selectedTag1 = ref("None");
+// const selectedTag2 = ref("None");
+const timeframe = ref("Day");
+const transactionType = ref("EXPENDITURE");
+const showFilters = ref(false);
+const graphData = ref(null);
+
+async function fetchGraphData(startDate, endDate, transactionType, timeframe) {
+  try {
+    const response = await axios.get("/api/transactions/graph-data/no-tags", {
+      params: {
+        startDate: startDate,
+        endDate: endDate,
+        transactionType: transactionType,
+        dateFormat: timeframe,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching graph data:", error);
+    return error.response.data;
+  }
+}
+
+const createChart = async () => {
+  graphData.value = await fetchGraphData(
+    startDate.value,
+    endDate.value,
+    transactionType.value,
+    timeframe.value
+  );
+};
 
 const incomeExpense = ref("Income");
-// eslint-disable-next-line no-unused-vars
 const currentMonth = new Date().toLocaleString("default", { month: "long" });
 const selectedMonth = ref(currentMonth);
 const months = [
@@ -88,6 +212,8 @@ const toggleIncomeExpense = () => {
 const incomeExpenseMapped = computed(() => {
   return incomeExpense.value === "Income" ? "INCOME" : "EXPENDITURE";
 });
+
+const compareText = ref("+ Compare");
 
 function mapMonthsToNumbers(month) {
   const months = [
@@ -121,6 +247,13 @@ const fetchAmount = async () => {
     return 0;
   }
 };
+
+const toggleCompare = () => {
+  showFilters.value = !showFilters.value;
+  compareText.value =
+    compareText.value === "+ Compare" ? "- Compare" : "+ Compare";
+};
+
 const tagStats = ref({});
 const fetchTagStats = async () => {
   try {
@@ -134,9 +267,56 @@ const fetchTagStats = async () => {
     return 0;
   }
 };
+// Fetch graph data when component is mounted
+onMounted(async () => {
+  graphData.value = await fetchGraphData(
+    startDate.value,
+    endDate.value,
+    transactionType.value,
+    timeframe.value
+  );
+});
 
 watch([selectedMonth, incomeExpense], fetchAmount, { immediate: true });
 watch([selectedMonth, incomeExpense], fetchTagStats, { immediate: true });
+</script>
+<script>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "BarChart",
+  // eslint-disable-next-line vue/no-unused-components
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: ["January", "February", "March"],
+        datasets: [{ data: [40, 20, 12] }],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+};
 </script>
 <style scoped>
 .navbar {
@@ -194,12 +374,39 @@ watch([selectedMonth, incomeExpense], fetchTagStats, { immediate: true });
 }
 
 .filters {
-  height: 100px; /* Adjust as needed */
+  align-items: center;
+  height: 200px; /* Adjust as needed */
   background-color: #ffffff;
 }
 
+.top-row,
+.bottom-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+
+.filters input[type="date"],
+.filters select {
+  flex: 1;
+  margin: 0 10px;
+}
+
+.third-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+
+.third-row button {
+  flex: 1;
+  margin: 0 10px;
+}
+
 .graph {
-  height: 450px; /* Adjust as needed */
+  height: 550px; /* Adjust as needed */
   background-color: #ffffff;
   margin-top: 10px;
 }
